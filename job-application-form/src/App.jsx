745fca617f581f156367723fcc3f83f98575b5cf
Elliot <elliotgriffiss@gmail.com>
2022-04-20 15:58:09 +0100

    Installed Roboto font using npm fontsource

--- job-application-form/src/App.jsx
+++ job-application-form/src/App.jsx
@@ -1,4 +1,8 @@
 import Button from "@mui/material/Button";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return <Button variant="contained">Hello World</Button>;
